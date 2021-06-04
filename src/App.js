b84a4142cd90a51066fcf1d1a172e0265fd4208e
DJGARG7 <18BCE052@NIRMAUNI.AC.IN>
2021-06-04 22:48:01 +0530

    trying out something with sockets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+// import { io } from "socket.io-client";
 import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
+  // const socket = io("http://localhost:5000");
+  // socket.on("connect", () => {
+  //   console.log(socket.id);
+  // });
   const expenses = [
     {
       id: "e1",
